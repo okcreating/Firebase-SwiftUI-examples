f41ea164a30a0c136aeeabdf48319ce59d2ad345
okcreating <oksibearus@gmail.com>
2025-03-20 10:55:43 +0000

    Initial Commit

---
+++ Firebase+SwiftUI examples/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Firebase+SwiftUI examples
+//
+//  Created by Oksana Kazarinova on 20/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Firebase+SwiftUI examples/Firebase_SwiftUI_examplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_SwiftUI_examplesApp.swift
+//  Firebase+SwiftUI examples
+//
+//  Created by Oksana Kazarinova on 20/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_SwiftUI_examplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
